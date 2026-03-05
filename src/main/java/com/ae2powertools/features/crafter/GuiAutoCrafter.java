4ef9c7a73696b31edf763b74e7fa9da1afc9948b
NuanKi <emvoh@proton.me>
2026-03-05 05:15:54 -0600

    Add localized strict/ignored NBT matching tooltips

--- src/main/java/com/ae2powertools/features/crafter/GuiAutoCrafter.java
+++ src/main/java/com/ae2powertools/features/crafter/GuiAutoCrafter.java
@@ -529,11 +529,13 @@ public class GuiAutoCrafter extends GuiContainer {
             tooltip.add("");
             tooltip.add(TextFormatting.DARK_GRAY + I18n.format("gui.ae2powertools.crafter.speed.explanation"));
         } else if (ignoreNbtBtnHovered) {
-            boolean on = isIgnoreNbtEnabled(getCurrentPage());
-            tooltip.add(I18n.format(on
+            boolean ignored = isIgnoreNbtEnabled(getCurrentPage());
+            tooltip.add(I18n.format(ignored
                     ? "gui.ae2powertools.crafter.nbt_matching.ignored"
                     : "gui.ae2powertools.crafter.nbt_matching.strict"));
-            tooltip.add(TextFormatting.DARK_GRAY + I18n.format("gui.ae2powertools.crafter.ignore_nbt.desc"));
+            tooltip.add(TextFormatting.DARK_GRAY + I18n.format(ignored
+                    ? "gui.ae2powertools.crafter.nbt_matching.ignored.desc"
+                    : "gui.ae2powertools.crafter.nbt_matching.strict.desc"));
         }
 
         if (!tooltip.isEmpty()) {
