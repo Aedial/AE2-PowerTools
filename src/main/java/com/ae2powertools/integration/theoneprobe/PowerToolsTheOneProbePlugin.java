9b58297f45d2b6c3a4d3a2183e09ea6be70adc2f
Aedial <aedial.dev@gmail.com>
2026-08-04 01:52:47 +0200

    Extend the TOP/WAILA probe

--- src/main/java/com/ae2powertools/integration/theoneprobe/PowerToolsTheOneProbePlugin.java
+++ src/main/java/com/ae2powertools/integration/theoneprobe/PowerToolsTheOneProbePlugin.java
@@ -1,5 +1,6 @@
 package com.ae2powertools.integration.theoneprobe;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 import net.minecraft.block.state.IBlockState;
@@ -7,6 +8,9 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+import appeng.api.parts.IPart;
+import appeng.integration.modules.theoneprobe.part.PartAccessor;
 
 import mcjty.theoneprobe.api.IProbeHitData;
 import mcjty.theoneprobe.api.IProbeInfo;
@@ -19,9 +23,16 @@ import com.ae2powertools.features.crafter.AutoCrafterProbeHelper;
 import com.ae2powertools.features.crafter.TileAutoCrafter;
 import com.ae2powertools.features.maintainer.MaintainerProbeHelper;
 import com.ae2powertools.features.maintainer.TileBetterLevelMaintainer;
+import com.ae2powertools.features.monitor.StorageMonitorProbeHelper;
+import com.ae2powertools.features.monitor.StorageMonitorProbeHelper.AlarmProbeData;
+import com.ae2powertools.features.monitor.StorageMonitorProbeHelper.EmitterProbeData;
+import com.ae2powertools.features.monitor.alarm.TileLevelMonitorAlarm;
+import com.ae2powertools.features.monitor.emitter.IEmitterCardHost;
 
 
 public class PowerToolsTheOneProbePlugin implements Function<ITheOneProbe, Void>, IProbeInfoProvider {
+
+    private final PartAccessor partAccessor = new PartAccessor();
 
     public static void register() {
         FMLInterModComms.sendFunctionMessage(
@@ -49,17 +60,43 @@ public class PowerToolsTheOneProbePlugin implements Function<ITheOneProbe, Void>
                              World world,
                              IBlockState blockState,
                              IProbeHitData data) {
-        TileEntity tile = world.getTileEntity(data.getPos());
         if (probeInfo == null) return;
 
+        TileEntity tile = world.getTileEntity(data.getPos());
+        if (appendTileProbeInfo(probeInfo, player, tile)) return;
+        if (tile == null) return;
+
+        Optional<IPart> maybePart = this.partAccessor.getMaybePart(tile, data);
+        if (!maybePart.isPresent()) return;
+
+        IPart part = maybePart.get();
+        if (part instanceof IEmitterCardHost) {
+            addEmitterInfo(probeInfo, (IEmitterCardHost) part);
+        }
+    }
+
+    private boolean appendTileProbeInfo(IProbeInfo probeInfo, EntityPlayer player, TileEntity tile) {
         if (tile instanceof TileAutoCrafter) {
             addAutoCrafterInfo(probeInfo, (TileAutoCrafter) tile);
-            return;
+            return true;
         }
 
         if (tile instanceof TileBetterLevelMaintainer) {
             addMaintainerInfo(probeInfo, (TileBetterLevelMaintainer) tile);
+            return true;
         }
+
+        if (tile instanceof IEmitterCardHost) {
+            addEmitterInfo(probeInfo, (IEmitterCardHost) tile);
+            return true;
+        }
+
+        if (tile instanceof TileLevelMonitorAlarm) {
+            addAlarmInfo(probeInfo, player, (TileLevelMonitorAlarm) tile);
+            return true;
+        }
+
+        return false;
     }
 
     private void addAutoCrafterInfo(IProbeInfo probeInfo, TileAutoCrafter crafter) {
@@ -94,7 +131,43 @@ public class PowerToolsTheOneProbePlugin implements Function<ITheOneProbe, Void>
         probeInfo.text("");
 
         probeInfo.element(new TopTranslatedTextElement(
+            MaintainerProbeHelper.STATUS_CPU_TOOLTIP_KEY,
+            probeData.getCpuStatusArgs()));
+
+        probeInfo.element(new TopTranslatedTextElement(
+            MaintainerProbeHelper.STATUS_RECIPE_TOOLTIP_KEY,
+            probeData.getRecipeStatusArgs()));
+
+        probeInfo.element(new TopTranslatedTextElement(
             probeData.getTimingKey(),
             probeData.getTimingArgs()));
+    }
+
+    private void addEmitterInfo(IProbeInfo probeInfo, IEmitterCardHost emitter) {
+        EmitterProbeData probeData = StorageMonitorProbeHelper.collectEmitterData(emitter);
+        if (!probeData.isValid()) return;
+
+        // Separation line
+        probeInfo.text("");
+
+        if (probeData.hasFuzzyCard()) {
+            probeInfo.element(new TopTranslatedTextElement(StorageMonitorProbeHelper.EMITTER_FUZZY_CARD_TOOLTIP_KEY));
+        }
+
+        if (probeData.hasCraftingCard()) {
+            probeInfo.element(new TopTranslatedTextElement(StorageMonitorProbeHelper.EMITTER_CRAFTING_CARD_TOOLTIP_KEY));
+        }
+
+        probeInfo.element(new TopTranslatedTextElement(probeData.getRedstoneModeKey()));
+    }
+
+    private void addAlarmInfo(IProbeInfo probeInfo, EntityPlayer player, TileLevelMonitorAlarm alarm) {
+        AlarmProbeData probeData = StorageMonitorProbeHelper.collectAlarmData(alarm, player);
+        if (!probeData.isValid()) return;
+
+        // Separation line
+        probeInfo.text("");
+
+        probeInfo.element(new TopTranslatedTextElement(probeData.getRegistrationKey()));
     }
 }
--- src/main/java/com/ae2powertools/integration/waila/PowerToolsWailaModule.java
+++ src/main/java/com/ae2powertools/integration/waila/PowerToolsWailaModule.java
@@ -1,6 +1,7 @@
 package com.ae2powertools.integration.waila;
 
 import java.util.List;
+import java.util.Optional;
 
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -8,10 +9,15 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
+import appeng.api.parts.IPart;
 import appeng.integration.modules.waila.BaseWailaDataProvider;
+import appeng.integration.modules.waila.part.PartAccessor;
+import appeng.integration.modules.waila.part.Tracer;
+import appeng.tile.AEBaseTile;
 
 import mcp.mobius.waila.api.IWailaConfigHandler;
 import mcp.mobius.waila.api.IWailaDataAccessor;
@@ -21,6 +27,11 @@ import com.ae2powertools.features.crafter.AutoCrafterProbeHelper;
 import com.ae2powertools.features.crafter.TileAutoCrafter;
 import com.ae2powertools.features.maintainer.MaintainerProbeHelper;
 import com.ae2powertools.features.maintainer.TileBetterLevelMaintainer;
+import com.ae2powertools.features.monitor.StorageMonitorProbeHelper;
+import com.ae2powertools.features.monitor.StorageMonitorProbeHelper.AlarmProbeData;
+import com.ae2powertools.features.monitor.StorageMonitorProbeHelper.EmitterProbeData;
+import com.ae2powertools.features.monitor.alarm.TileLevelMonitorAlarm;
+import com.ae2powertools.features.monitor.emitter.IEmitterCardHost;
 
 
 public final class PowerToolsWailaModule {
@@ -35,8 +46,18 @@ public final class PowerToolsWailaModule {
     }
 
     public static void register(IWailaRegistrar registrar) {
-        registrar.registerBodyProvider(new AutoCrafterWailaDataProvider(), TileAutoCrafter.class);
-        registrar.registerBodyProvider(new MaintainerWailaDataProvider(), TileBetterLevelMaintainer.class);
+        AutoCrafterWailaDataProvider autoCrafterProvider = new AutoCrafterWailaDataProvider();
+        MaintainerWailaDataProvider maintainerProvider = new MaintainerWailaDataProvider();
+        StorageMonitorWailaDataProvider storageMonitorProvider = new StorageMonitorWailaDataProvider();
+
+        registrar.registerBodyProvider(autoCrafterProvider, TileAutoCrafter.class);
+        registrar.registerNBTProvider(autoCrafterProvider, TileAutoCrafter.class);
+
+        registrar.registerBodyProvider(maintainerProvider, TileBetterLevelMaintainer.class);
+        registrar.registerNBTProvider(maintainerProvider, TileBetterLevelMaintainer.class);
+
+        registrar.registerBodyProvider(storageMonitorProvider, AEBaseTile.class);
+        registrar.registerNBTProvider(storageMonitorProvider, AEBaseTile.class);
     }
 
     private static final class AutoCrafterWailaDataProvider extends BaseWailaDataProvider {
@@ -103,6 +124,14 @@ public final class PowerToolsWailaModule {
             currentToolTip.add("");
 
             currentToolTip.add(I18n.format(
+                MaintainerProbeHelper.STATUS_CPU_TOOLTIP_KEY,
+                (Object[]) probeData.getCpuStatusArgs()));
+
+            currentToolTip.add(I18n.format(
+                MaintainerProbeHelper.STATUS_RECIPE_TOOLTIP_KEY,
+                (Object[]) probeData.getRecipeStatusArgs()));
+
+            currentToolTip.add(I18n.format(
                 probeData.getTimingKey(),
                 (Object[]) probeData.getTimingArgs()));
 
@@ -117,6 +146,73 @@ public final class PowerToolsWailaModule {
                                          BlockPos pos) {
             if (te instanceof TileBetterLevelMaintainer) {
                 MaintainerProbeHelper.writeWailaData((TileBetterLevelMaintainer) te, tag);
+            }
+
+            return tag;
+        }
+    }
+
+    private static final class StorageMonitorWailaDataProvider extends BaseWailaDataProvider {
+
+        private final PartAccessor partAccessor = new PartAccessor();
+        private final Tracer tracer = new Tracer();
+
+        @Override
+        public List<String> getWailaBody(ItemStack itemStack,
+                                         List<String> currentToolTip,
+                                         IWailaDataAccessor accessor,
+                                         IWailaConfigHandler config) {
+            NBTTagCompound tag = accessor.getNBTData();
+
+            EmitterProbeData emitterProbeData = StorageMonitorProbeHelper.readEmitterWailaData(tag);
+            if (emitterProbeData.isValid()) {
+                currentToolTip.add("");
+
+                if (emitterProbeData.hasFuzzyCard()) {
+                    currentToolTip.add(I18n.format(StorageMonitorProbeHelper.EMITTER_FUZZY_CARD_TOOLTIP_KEY));
+                }
+
+                if (emitterProbeData.hasCraftingCard()) {
+                    currentToolTip.add(I18n.format(StorageMonitorProbeHelper.EMITTER_CRAFTING_CARD_TOOLTIP_KEY));
+                }
+
+                currentToolTip.add(I18n.format(emitterProbeData.getRedstoneModeKey()));
+                return currentToolTip;
+            }
+
+            AlarmProbeData alarmProbeData = StorageMonitorProbeHelper.readAlarmWailaData(tag);
+            if (!alarmProbeData.isValid()) return currentToolTip;
+
+            currentToolTip.add("");
+            currentToolTip.add(I18n.format(alarmProbeData.getRegistrationKey()));
+            return currentToolTip;
+        }
+
+        @Override
+        public NBTTagCompound getNBTData(EntityPlayerMP player,
+                                         TileEntity te,
+                                         NBTTagCompound tag,
+                                         World world,
+                                         BlockPos pos) {
+            if (te instanceof IEmitterCardHost) {
+                StorageMonitorProbeHelper.writeEmitterWailaData((IEmitterCardHost) te, tag);
+                return tag;
+            }
+
+            if (te instanceof TileLevelMonitorAlarm) {
+                StorageMonitorProbeHelper.writeAlarmWailaData((TileLevelMonitorAlarm) te, player, tag);
+                return tag;
+            }
+
+            RayTraceResult hit = this.tracer.retraceBlock(world, player, pos);
+            if (hit == null) return tag;
+
+            Optional<IPart> maybePart = this.partAccessor.getMaybePart(te, hit);
+            if (!maybePart.isPresent()) return tag;
+
+            IPart part = maybePart.get();
+            if (part instanceof IEmitterCardHost) {
+                StorageMonitorProbeHelper.writeEmitterWailaData((IEmitterCardHost) part, tag);
             }
 
             return tag;
